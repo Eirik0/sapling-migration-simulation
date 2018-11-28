6092cfcdc3d6bf2144435e9e999eee6019856aa4
Eirik0 <eirik@z.cash>
2018-11-28 15:30:53 -0700

    Add Sapling activation

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# sapling-migration-simulation
+# Sapling Migration Simulation
+
+This script is intended to simulate strategies for migrating funds from the
+Sprout to Sapling shielded pools on the Zcash blockchain.
+
+## How it works
+
+First we generate a mix of mock shielded and transparant transactions up to a
+given Sapling activation height. We then continue to generate blocks in a
+similar fashion but at the same time for each user/node we execute a stragegy
+for migrating Sprout shielded addresses to Sapling shielded addresses.
+The script generates a minimal amount of information to execute and validate
+the strategy.
+
+As part of execution we write 3 files:
+blockvhain.csv:
+    This csv file has four columns: "block_height", "input", "output", and
+    "amount".
+    This file is a simplification of the information stored on the block chain.
+    "input" and "output" represent transaction ids and are formatted in a way
+    such that we can figure out the type of transaction, and which user the
+    amounts belongs to. In the actual Zcash block chain, some of this
+    information is obfuscated, but for the sake of the validating the migration
+    stragegy in this simulation, it is designed to not hide any infomation.
+
+user_balance_*.csv:
+    These csv files have four columns: "user_id", "sprout_balance",
+    "sapling_balance", and "transparent_balance".
+    These files are intended to provide some insight to the mock data which is
+    generated, and to aid in validating the migration stragegy. One file is
+    produced before the migration begins, and a second file is produced after
+    the simulation ends.
+
+## Usage
+
+After cloning, enter the following command to run the script and generate the
+output files:
+```
+python simulator.py
+```
--- simulator.py
+++ simulator.py
@@ -3,19 +3,23 @@
 import random
 
 
-NUM_BLOCKS = 400
+SAPLING_ACTIVATION_HEIGHT = 400
+SIMULATION_END_HEIGHT = 600
 ZEC_PER_BLOCK = 10
 SHIELDED_ODDS = 0.2  # Assume 20% of txs are shielded
 
 # TODO Add tiers of users
 NUM_USERS = 10
 
+SPROUT_PREXIX = "x"
+SAPLING_PREFIX = "z"
+TRANSPARENT_PREFIX = "t"
 
-# TODO: consider tx type ?
+
 class Transaction:
     def __init__(self, block_height, input, output, amount):
         self.block_height = block_height
-        # tx_id = ("s|t" + user_id + "_" + tx_num) or ("cb_" + cb_index)
+        # tx_id = (PREFIX + "_" + user_id + "_" + tx_num) or ("cb_" + cb_index)
         self.input = input
         self.output = output
         self.amount = amount
@@ -24,18 +28,62 @@ class Transaction:
 class User:
     def __init__(self, user_id):
         self.user_id = user_id
-        self.shielded_amounts = []
+        self.sprout_amounts = []
+        self.sapling_amounts = []
         self.transparent_amounts = []
         self.num_txs = 0
 
-    def add_amount(self, is_shielded, amount):
+    def add_amount(self, is_shielded, amount, is_sapling):
         if is_shielded:
-            tx_id = "s" + str(self.user_id) + "_" + str(len(self.shielded_amounts))
-            self.shielded_amounts.append(amount)
+            if is_sapling:
+                tx_id = SAPLING_PREFIX + str(self.user_id) + "_" + str(len(self.sapling_amounts))
+                self.sapling_amounts.append(amount)
+            else:
+                tx_id = SPROUT_PREXIX + str(self.user_id) + "_" + str(len(self.sprout_amounts))
+                self.sprout_amounts.append(amount)
         else:
-            tx_id = "t" + str(self.user_id) + "_" + str(len(self.transparent_amounts))
+            tx_id = TRANSPARENT_PREFIX + str(self.user_id) + "_" + str(len(self.transparent_amounts))
             self.transparent_amounts.append(amount)
         return tx_id
+
+
+def distribute_coinbase_transactions(block_height, users, transactions, is_sapling):
+    coinbase_amount = ZEC_PER_BLOCK
+    cb_index = 0
+    while coinbase_amount > 0:
+        # Distriburte random amount to random user
+        if coinbase_amount > 0.5:
+            distibution_amount = random.uniform(0, coinbase_amount)
+        else:
+            distibution_amount = coinbase_amount
+
+        user = users[random.randint(0, NUM_USERS - 1)]
+        is_shielded = random.uniform(0, 1) <= SHIELDED_ODDS
+        tx_id = user.add_amount(is_shielded, distibution_amount, is_sapling)
+
+        # Append tx to list
+        transactions.append(Transaction(block_height, "cb_" + str(cb_index), tx_id, distibution_amount))
+
+        coinbase_amount -= distibution_amount
+        cb_index += 1
+
+
+def write_user_balance_file(users, before_sapling_activation):
+    if before_sapling_activation:
+        sorted_users = sorted(users, key=lambda user: sum(user.sprout_amounts), reverse=True)
+        user_balance_file_name = "user_balance_sprout.csv"
+    else:
+        sorted_users = sorted(users, key=lambda user: sum(user.sapling_amounts), reverse=True)
+        user_balance_file_name = "user_balance_sapling.csv"
+
+    user_balance_file = open(user_balance_file_name, "w+")
+    user_balance_file.write("user_id,sprout_balance,sapling_balance,transparent_balance\n")
+
+    for user in sorted_users:
+        user_balance_file.write("{},{},{},{}\n".format(user.user_id, sum(user.sprout_amounts), sum(user.sapling_amounts), sum(user.transparent_amounts)))
+
+    user_balance_file.flush()
+    user_balance_file.close()
 
 
 def main():
@@ -45,27 +93,18 @@ def main():
         users.append(User(user_id))
 
     transactions = []
-    # Generate block data
-    for block_height in xrange(1, NUM_BLOCKS):
-        # Distribute coinbase
-        coinbase_amount = ZEC_PER_BLOCK
-        cb_index = 0
-        while coinbase_amount > 0:
-            # Distriburte random amount to random user
-            if coinbase_amount > 0.5:
-                distibution_amount = random.uniform(0, coinbase_amount)
-            else:
-                distibution_amount = coinbase_amount
 
-            user = users[random.randint(0, NUM_USERS - 1)]
-            is_shielded = random.uniform(0, 1) <= SHIELDED_ODDS
-            tx_id = user.add_amount(is_shielded, distibution_amount)
+    # Generate Sprout block data
+    for block_height in xrange(1, SAPLING_ACTIVATION_HEIGHT):
+        distribute_coinbase_transactions(block_height, users, transactions, False)
 
-            # Append tx to list
-            transactions.append(Transaction(block_height, "cb_" + str(cb_index), tx_id, distibution_amount))
+    write_user_balance_file(users, True)
 
-            coinbase_amount -= distibution_amount
-            cb_index += 1
+    # Generate Sapling block data
+    for block_height in xrange(SAPLING_ACTIVATION_HEIGHT, SIMULATION_END_HEIGHT):
+        distribute_coinbase_transactions(block_height, users, transactions, True)
+
+    write_user_balance_file(users, False)
 
     blockchain_file = open("blockchain.csv", "w+")
     blockchain_file.write("block_height,input,output,amount\n")
